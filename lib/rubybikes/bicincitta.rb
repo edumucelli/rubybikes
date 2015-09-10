66543ed007d141622f42b8b57f602c285700fef7
eduardom <edumucelli@gmail.com>
2015-09-11 01:58:52 +0200

    Update bicincitta

--- lib/rubybikes/bicincitta.rb
+++ lib/rubybikes/bicincitta.rb
@@ -9,10 +9,10 @@ META = {'system' => 'Bicincittà', 'company' => 'Comunicare S.r.l.'}
 
 class BicincittaOld < BikeShareSystem
 
-    # This is the worst thing ever made, it is impossible to believe
-    # that someone had this idea of representing the latitudes, longitudes
-    # names, and availability with a string separated by '_'
-    # Look at this string "1000000000xxxxxxxxxxxxxxxxxxxx" and tell me how
+    # This is the one of worst things ever made in the computer history, it is 
+    # impossible to believe that someone had this idea of representing the latitudes, 
+    # longitudes, names, and availability with a string separated by '_'.
+    # Furthermore, look at this string "1000000000xxxxxxxxxxxxxxxxxxxx" and tell me how
     # many available bike stands this station has. Who had the brilliant
     # idea to model the number of zeros as the number of available stands?
     # Thanks to eskerda that figured that out.
@@ -20,7 +20,7 @@ class BicincittaOld < BikeShareSystem
     # One caveat here in this mess is that the values after the '+'
     # point to parameters of a bike stand in Rome, which has no relation with
     # each of the cities whatsoever! I wonder what they mean with that. Besides,
-    # If you go on the Google Street View and check that position (41.900074, 12.476478),
+    # if you go on the Google Street View and check that position (41.900074, 12.476478),
     # that is close by a statue in Rome, which probaly names the stand in the system, 'Maddalena'.
     # Still, there is no bike station there! I am pretty sure this is some kind of voodoo-like
     # thing made by the (sic) programmer who made this shi...bicincitta.
@@ -30,9 +30,9 @@ class BicincittaOld < BikeShareSystem
     # Previously (well, it is pending my pull-request to PyBikes), the get_hash
     # method considered only latitude and longitude. As I've included the name on
     # that I thought that no bike stand would ever have the same name, latitude and longitude
-    # parameters. Then this 'Maddalena' appeared like that! On different cities the same
-    # station, but on the map it is possible to see that the location has nothing to do
-    # with the rest from the respective city.
+    # parameters. Then this 'Maddalena' appeared like that! Different cities, but the same
+    # station. On the map it is possible to see that the location has nothing to do
+    # with the rest of the bike stands from the respective city.
 
     LAT_RGX         = /var sita_x =.*?\"(.*?)\"\+.*?;/
     LNG_RGX         = /var sita_y =.*?\"(.*?)\"\+.*?;/
@@ -69,50 +69,55 @@ class BicincittaOld < BikeShareSystem
     end
 end
 
+class Bicincitta < BikeShareSystem
 
-# class Bicincitta(BikeShareSystem):
-#     sync = True
-#     _RE_INFO="RefreshMap\((.*?)\)\;"
-#     _endpoint = "http://bicincitta.tobike.it/frmLeStazioni.aspx?ID={id}"
+    INFO_RGX    = /RefreshMap\('(.*?)'\)\}/
+    URL         = "http://bicincitta.tobike.it/frmLeStazioni.aspx?ID=%{id}"
 
-#     def __init__(self, tag, meta, ** instance):
-#         super(Bicincitta, self).__init__(tag, meta)
-
-#         if 'endpoint' in instance:
-#             endpoint = instance['endpoint']
-#         else:
-#             endpoint = Bicincitta._endpoint
-
-#         if 'system_id' in instance:
-#             self.system_id = system_id
-#             self.url = [endpoint.format(id = system_id)]
-#         elif 'comunes' in instance:
-#             self.url = map(
-#                 lambda comune: endpoint.format(id = comune['id']),
-#                 instance['comunes']
-#             )
-#         else:
-#             self.url = [endpoint]
-
-
-#     def update(self, scraper = None):
-#         if scraper is None:
-#             scraper = utils.PyBikesScraper()
-#         self.stations = []
-#         for url in self.url:
-#             self.stations += Bicincitta._getComuneStations(url, scraper)
-
-#     @staticmethod
-#     def _getComuneStations(url, scraper):
-#         data = scraper.request(url)
-#         raw  = re.findall(Bicincitta._RE_INFO, data)
-#         info = raw[0].split('\',\'')
-#         info = map(lambda chunk: chunk.split('|'), info)
-#         # Yes, this is a joke
-#         return [ BicincittaStation(name, desc, float(lat), float(lng),
-#                  stat.count('4'), stat.count('0')) for name, desc, lat, lng,
-#                  stat in zip(info[5], info[7], info[3], info[4], info[6]) ]
-
+    attr_accessor :stations, :meta
+    def initialize(schema_instance_parameters={})
+        tag       = schema_instance_parameters.fetch('tag')
+        meta      = schema_instance_parameters.fetch('meta')
+        if schema_instance_parameters.has_key? 'comunes'
+            @feed_url = schema_instance_parameters['comunes'].map {|comune| URL % {:id => comune['id']}}
+        else
+            @feed_url = schema_instance_parameters.fetch('feed_url')
+        end
+        puts @feed_url
+        @meta     = meta.merge(META)
+        super(tag, @meta)
+    end
+    def update
+        scraper = Scraper.new()
+        stations = []
+        if @feed_url.is_a? String
+            html = scraper.request(@feed_url)
+            @stations = process_stations(html)
+        else
+            @feed_url.each do |url|
+                html = scraper.request(url)
+                @stations.push(*process_stations(html))
+            end
+        end
+    end
+    def process_stations(html)
+        stations = []
+        html.scan(INFO_RGX).each do |info|
+            mess = info[0].split("\',\'")
+            latitudes = mess[3].split('|')
+            longitudes = mess[4].split('|')
+            names = mess[5].split(':|')
+            availabilities = mess[6].split('|')
+            names.zip(latitudes, longitudes, availabilities).each do |name, latitude, longitude, availability|
+                bikes = availability.count('4')
+                free = availability.count('0')
+                station = BicincittaStation.new(name, latitude.to_f, longitude.to_f, bikes, free)
+                stations << station
+            end
+        end
+        stations
+    end
+end
 class BicincittaStation < BikeShareStation
     def initialize(name, latitude, longitude, bikes, free)
         super()
@@ -126,17 +131,43 @@ end
 
 if __FILE__ == $0
     instance = {
-        "tag" => "alba",
-        "system_id" => 15,
-        "meta" => {
-            "name" => "Alba",
-            "city" => "Alba",
-            "country" => "IT",
-            "latitude" => 44.716667,
-            "longitude" => 8.083333
-        }
+                    "comunes" => [
+                        {
+                            "id" => 22,
+                            "name" => "Torino"
+                        },
+                        {
+                            "id" => 61,
+                            "name" => "Grugliasco"
+                        },
+                        {
+                            "id" => 62,
+                            "name" => "Collegno"
+                        },
+                        {
+                            "id" => 63,
+                            "name" => "Venaria Reale"
+                        },
+                        {
+                            "id" => 64,
+                            "name" => "Alpignano"
+                        },
+                        {
+                            "id" => 65,
+                            "name" => "Druento"
+                        }
+                    ],
+                    "meta" => {
+                        "latitude" => 45.07098200000001,
+                        "city" => "Torino",
+                        "name" => "[TO]BIKE",
+                        "longitude" => 7.685676,
+                        "country" => "IT"
+                    },
+                    "feed_url" => "http://www.tobike.it/frmLeStazioni.aspx?ID={id}",
+                    "tag" => "to-bike"
     }
-    cyclocity = BicincittaOld.new(instance)
+    cyclocity = Bicincitta.new(instance)
     cyclocity.update
     puts cyclocity.stations.length
     cyclocity.stations.each do |station|
